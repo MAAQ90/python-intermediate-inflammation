234833fbec6814519e764c5a7eb86ff004b7b1b9
MAAQ90 <mohammadaliausaf@gmail.com>
2025-04-02 14:51:20 +0100

    Add patient_normalise function and tests

--- inflammation/models.py
+++ inflammation/models.py
@@ -9,7 +9,6 @@ and each column represents a single day across all patients.
 
 import numpy as np
 
-
 def load_csv(filename):  
     """Load a Numpy array from a CSV
 
@@ -17,11 +16,9 @@ def load_csv(filename):
     """
     return np.loadtxt(fname=filename, delimiter=',')
 
-
 def daily_mean(data):
     """Calculate the daily mean of a 2d inflammation data array."""
     return np.mean(data, axis=0)
-
 
 def daily_max(data):
     """Calculate the daily max of a 2d inflammation data array."""
@@ -32,3 +29,7 @@ def daily_min(data):
     """Calculate the daily min of a 2d inflammation data array."""
     return np.min(data, axis=0)
 
+def patient_normalise(data):
+    """Normalise patient data from a 2D inflammation data array."""
+    max = np.max(data, axis=0)
+    return data / max[:, np.newaxis]
--- tests/test_models.py
+++ tests/test_models.py
@@ -69,3 +69,20 @@ def test_daily_min_string():
         error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
 
 
+
+
+from inflammation.models import patient_normalise
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]),
+        ([[1, 1, 1], [1, 1, 1], [1, 1, 1]], [[1, 1, 1], [1, 1, 1], [1, 1, 1]]),
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]]),
+    ])
+def test_patient_normalise(test, expected):
+    """Test normalisation works for arrays of one and positive integers.
+       Test with a relative and absolute tolerance of 0.01."""
+
+    result = patient_normalise(np.array(test))
+    npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
