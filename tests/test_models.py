2619903dab33fc59196ecd31e7e184284c3d8699
MAAQ90 <mohammadaliausaf@gmail.com>
2025-04-02 09:51:50 +0100

    #

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,7 +3,7 @@
 import numpy as np
 import numpy.testing as npt
 
-from inflammation.models import daily_mean
+from inflammation.models import daily_mean, daily_max, daily_min
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -28,4 +28,41 @@ def test_daily_mean_integers():
 
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
+
+def test_daily_max_zeros():
+
+    test_input = np.array([[0, 0],
+                           [0, 0],
+                           [0, 0]])
+    test_result = np.array([0, 0])
+
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+def test_daily_max_integers():
+    
+    test_input = np.array([[1, 2],
+                           [3, 4],
+                           [5, 6]])
+    test_result = np.array([5, 6])
+
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+def test_daily_min_zeros():
+    
+    test_input = np.array([[0, 0],
+                           [0, 0],
+                           [0, 0]])
+    test_result = np.array([0, 0])
+
+    npt.assert_array_equal(daily_min(test_input), test_result)
+
+def test_daily_min_integers(): 
+    
+    test_daily_min_integers = np.array([[1, 2],
+                                        [3, 4],
+                                        [5, 6]])
+    test_result = np.array([1, 2]) 
+
+    npt.assert_array_equal(daily_min(test_daily_min_integers), test_result)
+
 
